17b2c4150cc8fa082331910c93d854dd21f70d47
Aditya251610 <aditya.sharma16062016@gmail.com>
2024-02-16 04:00:50 +0530

    checkpoint

--- src/components/addContact.js
+++ src/components/addContact.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
+//Adding contacts
 const AddContact = ({ addContactHandler, contacts }) => {
   const [state, setState] = useState({
     name: "",
